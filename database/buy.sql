26eb5b9b563e5d1009b0abd153c866c2471950fd
sjtees <109942106+sjtees@users.noreply.github.com>
2022-09-16 18:09:18 +0900

    Update buy.sql

--- database/buy.sql
+++ database/buy.sql
@@ -1,14 +1,14 @@
 create table buy(
 buy_no number primary key,
 customer_id varchar2(30) foreign key,
-product_no number foreign key,
-product_name varchar2(30) foreign key,
-product_type varchar2(30) foreign key,
-product_price number foreign key,
-product_color varchar2(10) foreign key,
-product_size number foreign key,
-product_cnt number foreign key,
-product_img varchar2(30) foreign key,
+item_no number foreign key,
+item_name varchar2(30) foreign key,
+item_type varchar2(30) foreign key,
+item_price number foreign key,
+item_color varchar2(10) foreign key,
+item_size number foreign key,
+item_cnt number foreign key,
+item_img varchar2(30) foreign key,
 buy_date date default sydate,
 buy_fee number not null
 
