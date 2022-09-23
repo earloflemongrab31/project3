1de046047042048b966274507c37d64a1782033d
hmk813 <109942509+hmk813@users.noreply.github.com>
2022-09-23 16:05:45 +0900

    Update center.sql

--- database/center.sql
+++ database/center.sql
@@ -1,10 +1,11 @@
-create table center(
-customer_id varchar2(30) primary key,
-center_notice varchar2(50) not null,
-center_no number not null,
-center_content varchar2(50) not null,
-center_qna varchar2(100) not null,
-center_date date default sysdate
+drop table center;
 
+create table center(
+center_no number not null primary key,
+customer_id references customer(customer_id),
+center_content varchar2(100) not null,
+center_qna varchar2(100) not null,
+center_date date default sysdate,
+center_title varchar2(30) not null
 
 );
