8b26b30a0a0c1b78fc354d9afd0e743452dd8ec2
hmk813 <109942509+hmk813@users.noreply.github.com>
2022-09-24 11:37:07 +0900

    Update review.sql

--- database/review.sql
+++ database/review.sql
@@ -1,6 +1,6 @@
 create table review(
-customer_id varchar2(30) foreign key,
-item_no number foreign key,
+customer_id references customer(customer_id),
+item_no references item(item_no),
 review_name varchar2(30) not null,
 review_content varchar2(100) not null,
 review_star number not null,
