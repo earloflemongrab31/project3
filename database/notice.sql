842fa9e6bae9052802fcdc6357828c68b2a28a05
kind0312 <109942628+kind0312@users.noreply.github.com>
2022-09-29 13:16:25 +0900

    Update notice.sql

--- database/notice.sql
+++ database/notice.sql
@@ -2,10 +2,11 @@ create table notice(
 admin_id references customer(customer_id) on delete set null, 
 notice_no number primary key, 
 notice_title varchar2(100) not null,
-notice_date date default sysdate, 
+notice_date date default sysdate,
+notice_update date,
 notice_read number default 0,
 notice_content varchar2(4000) not null, 
-notice_head char(6) check(notice_head = '긴급')
+notice_head varchar2(9) check(notice_head in('긴급','이벤트'))
 );
 
 create sequence notice_seq;
