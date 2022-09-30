8442df4bb544a7e785190880785f7b733c68169e
smy984 <106611123+smy984@users.noreply.github.com>
2022-09-30 16:05:32 +0900

    Update item(상품테이블).sql

--- database/item(상품테이블).sql
+++ database/item(상품테이블).sql
@@ -6,11 +6,10 @@ drop table item;
 create table item (
 item_no number primary key,
 item_name varchar2(30) not null,
-item_memo varchar2(16) not null,
 item_type varchar2(16) not null,
 item_price number not null,
 item_color varchar2(10) not null,
 item_size number not null,
-item_cnt number not null,
+item_total_cnt number not null,
 item_date date default sysdate,
 );
