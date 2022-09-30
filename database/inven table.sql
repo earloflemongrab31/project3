a5d21b98b0a63a6ecec9f4ad004a5410ffbfd81e
smy984 <106611123+smy984@users.noreply.github.com>
2022-09-30 16:13:35 +0900

    Create inven table.sql

---
+++ database/inven table.sql
@@ -0,0 +1,6 @@
+create table inven(
+item_no REFERENCES item(item_no) on delete cascade, 
+item_cnt number default 0 not null check(item_cnt >=0), 
+item_in date default sysdate, 
+item_out date
+);
