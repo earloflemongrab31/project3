414139b5fd4c4f6b9c6c71f6d83f7e32b9003351
earloflemongrab31 <57470776+earloflemongrab31@users.noreply.github.com>
2022-10-01 13:02:11 +0900

    Update cate table.sql

--- database/cate table.sql
+++ database/cate table.sql
@@ -1,5 +1,5 @@
 create table cate(
-cate_tier char(1) not null check(cate_tier in('1', '2')), 
+cate_tier char(1) check(cate_tier in('1', '2')), 
 cate_name varchar2(30) not null,
 cate_code char(6) primary key,  
 cate_parent references cate(cate_code)
