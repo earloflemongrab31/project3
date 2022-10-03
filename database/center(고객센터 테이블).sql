9b642c4bd23176f9d7921355eb970bd5842d11e6
kind0312 <109942628+kind0312@users.noreply.github.com>
2022-10-03 22:51:56 +0900

    Update and rename center.sql(고객센터 테이블) to center(고객센터 테이블).sql

--- database/center(고객센터 테이블).sql
+++ database/center(고객센터 테이블).sql
@@ -1,7 +1,7 @@
 drop table center;
 
 create table center(
-center_no number not null primary key,
+center_no number primary key,
 customer_id references customer(customer_id),
 center_content varchar2(100) not null,
 center_qna varchar2(100) not null,
