dd2bf54cbe2008bb7ea02327cfa6bf55cfb7734f
kind0312 <109942628+kind0312@users.noreply.github.com>
2022-10-04 15:25:10 +0900

    Update center(고객센터 테이블).sql

--- database/center(고객센터 테이블).sql
+++ database/center(고객센터 테이블).sql
@@ -4,7 +4,6 @@ create table center(
 center_no number primary key,
 customer_id references customer(customer_id),
 center_content varchar2(100) not null,
-center_qna varchar2(100) not null,
 center_date date default sysdate,
 center_title varchar2(30) not null
 
