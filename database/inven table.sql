cff11c6f2e9a255f6808d667f6ce363752860923
hmk813 <109942509+hmk813@users.noreply.github.com>
2022-10-07 17:52:25 +0900

    Update inven table.sql

--- database/inven table.sql
+++ database/inven table.sql
@@ -2,5 +2,7 @@ create table inven(
 item_no REFERENCES item(item_no) on delete cascade, 
 item_cnt number default 0 not null check(item_cnt >=0), 
 item_in date default sysdate, 
-item_out date
+item_out date,
+item_size varchar2(6),
+item_color varchar2(10)
 );
