0b4a634cd7faa0ceeaeb0c43382834a3774630fc
earloflemongrab31 <earloflemongrab31@gmail.com>
2022-10-14 16:25:40 +0900

    세미프로젝트 생성

---
+++ semiproject3/src/main/java/com/example/semiproject3/Semiproject3Application.java
@@ -0,0 +1,13 @@
+package com.example.semiproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semiproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semiproject3Application.class, args);
+	}
+
+}
