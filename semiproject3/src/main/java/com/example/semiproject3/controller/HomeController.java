ca63bace1f2605cdfc10d243ec6774f75023cfee
smy984 <smy984@naver.com>
2022-10-18 13:11:58 +0900

    브랜치 테스트

--- semiproject3/src/main/java/com/example/semiproject3/controller/HomeController.java
+++ semiproject3/src/main/java/com/example/semiproject3/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.example.semiproject3.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
+@RequestMapping("/main")
 public class HomeController {
 	
 	@GetMapping("/")
