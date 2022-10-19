2ef172759fcbae05dc1b006045c551020009a97a
earloflemongrab31 <earloflemongrab31@gmail.com>
2022-10-19 17:05:54 +0900

    cart dto dao dao  //회의하고 구현.

--- semiproject3/src/main/java/com/example/semiproject3/controller/ResearchController.java
+++ semiproject3/src/main/java/com/example/semiproject3/controller/ResearchController.java
@@ -28,10 +28,16 @@ public class ResearchController {
 		//아이디 중복방지
 		//(1) 1일경우 중복 0일경우 설문조사 참여가능
 		//(2) 아이디가 없을 경우 
-		int judge=researchDao.overlapId(loginId); 
-		if((judge>0) || (loginId==null)) {
+		int judge=researchDao.overlapId(loginId);
+		//아이디가 없다면 로그인화면으로 보내세요.
+		if(loginId==null) {
+			return "/customer/login";
+		}
+		//아이디가 있는데 이미 참여한 사람 
+		if(judge>0) {
 			return "redirect:confirm";
-			
+
+		//설문조사에 처음 참여하는 사람
 		}else
 			return "/research/insert";
 			
