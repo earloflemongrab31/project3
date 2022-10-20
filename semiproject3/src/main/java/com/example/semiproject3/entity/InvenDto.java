dcae5c5c7c4f47a71f740cf72adf8ceed98f95c4
hmk813 <moonkyus@hanmail.net>
2022-10-20 16:54:29 +0900

    재고 테이블 구현중

--- semiproject3/src/main/java/com/example/semiproject3/entity/InvenDto.java
+++ semiproject3/src/main/java/com/example/semiproject3/entity/InvenDto.java
@@ -5,9 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+
 public class InvenDto {
 	
-	private 
+	
 		
 }
