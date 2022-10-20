3fe408e4433fabff86363350937413286da15a9c
hmk813 <moonkyus@hanmail.net>
2022-10-20 16:29:38 +0900

    공지사항 페이징 구현 재설정

--- semiproject3/src/main/java/com/example/semiproject3/repository/ItemDaoImpl.java
+++ semiproject3/src/main/java/com/example/semiproject3/repository/ItemDaoImpl.java
@@ -2,16 +2,15 @@ package com.example.semiproject3.repository;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import com.example.semiproject3.entity.ImageDto;
 import com.example.semiproject3.entity.ItemDto;
 import com.example.semiproject3.vo.BuyListVO;
+import com.example.semiproject3.vo.ItemListSearchVO;
 
 @Repository
 public class ItemDaoImpl implements ItemDao {
@@ -208,6 +207,51 @@ public class ItemDaoImpl implements ItemDao {
 		String sql = "select * from buy_list_view where item_no = ?";
 		Object[] param = {itemNo};
 		return jdbcTemplate.query(sql, buyExtractor, param);
+	}
+
+	@Override
+	public List<ItemDto> selectList(ItemListSearchVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ItemDto> list(ItemListSearchVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ItemDto> search(ItemListSearchVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//카운트
+	@Override
+	public int count(ItemListSearchVO vo) {
+		if(vo.isSearch()) {//검색이라면
+			return searchCount(vo);
+		}
+		else { //목록이라면
+			return listCount(vo);
+		}
+	}
+
+	//검색 카운트
+	@Override
+	public int searchCount(ItemListSearchVO vo) {
+		String sql = "select count(*) from item where instr(#1, ?) > 0";
+		sql = sql.replace("#1", vo.getType());
+		Object[] param = {vo.getKeyword()};
+		return jdbcTemplate.queryForObject(sql, int.class, param);
+	}
+
+	//목록 카운트
+	@Override
+	public int listCount(ItemListSearchVO vo) {
+		String sql = "select count(*) from item";
+		return jdbcTemplate.queryForObject(sql, int.class);
 	}
 		
 }
