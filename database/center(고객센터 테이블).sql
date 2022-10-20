3c721e10b6bd1ce39838ad92a3c793619f68ce26
hmk813 <109942509+hmk813@users.noreply.github.com>
2022-10-20 11:08:18 +0900

    Update center(고객센터 테이블).sql

--- database/center(고객센터 테이블).sql
+++ database/center(고객센터 테이블).sql
@@ -2,9 +2,13 @@ drop table center;
 
 create table center(
 center_no number primary key,
-customer_id references customer(customer_id),
-center_content varchar2(100) not null,
-center_date date default sysdate,
-center_title varchar2(30) not null
-
+customer_id references customer(customer_id) on delete set null ,
+admin_id references admin(admin_id) on delete cascade,
+center_title varchar2(30),
+customer_content varchar2(4000),
+admin_content varchar2(4000),
+customer_date date default sysdate,
+admin_date date default sysdate
 );
+
+commit;
