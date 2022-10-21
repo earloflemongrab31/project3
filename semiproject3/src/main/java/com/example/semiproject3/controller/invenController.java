c6ea78d33c4b9d2335ff290636a41b8915938fa5
earloflemongrab31 <earloflemongrab31@gmail.com>
2022-10-21 15:45:26 +0900

    입출고테이블(수정중)

---
+++ semiproject3/src/main/java/com/example/semiproject3/controller/invenController.java
@@ -0,0 +1,49 @@
+package com.example.semiproject3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.semiproject3.entity.InvenDto;
+import com.example.semiproject3.repository.InvenDao;
+import com.example.semiproject3.repository.ItemDao;
+
+@Controller
+@RequestMapping("/warehouse")
+public class invenController {
+	
+	@Autowired
+	ItemDao itemDao;
+	@Autowired
+	InvenDao invenDao;
+	
+	@GetMapping("/itemList")
+	public String itemList(Model model) {
+		model.addAttribute("itemList",itemDao.selectList());
+		return "warehouse/itemList";
+	}
+	@GetMapping("/insert")
+	public String insert(
+			@RequestParam int itemNo,
+			Model model) {
+		//하나의 아이템정보를 가지고와서 화면에 뿌려준다. 
+		model.addAttribute("itemList",itemDao.selectOne(itemNo));
+		return "warehouse/insert";
+	}
+	@PostMapping("/insert")
+	public String insert(@ModelAttribute InvenDto invenDto) {
+		invenDao.insert(invenDto);
+		return "redirect:invenList";
+	}
+	@GetMapping("/invenList")
+	public String invenList(Model model) {
+		model.addAttribute("invenList",invenDao.selectList());
+		return "/warehouse/invenList";
+	}
+	
+}
