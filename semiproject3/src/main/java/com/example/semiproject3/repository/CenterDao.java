630870e695c2fc3650c53078d89edeae2196b125
smy984 <smy984@naver.com>
2022-10-24 12:05:01 +0900

    고객센터 페이징 구현 중

--- semiproject3/src/main/java/com/example/semiproject3/repository/CenterDao.java
+++ semiproject3/src/main/java/com/example/semiproject3/repository/CenterDao.java
@@ -3,6 +3,7 @@ package com.example.semiproject3.repository;
 import java.util.List;
 
 import com.example.semiproject3.entity.CenterDto;
+import com.example.semiproject3.vo.CenterListSearchVO;
 
 public interface CenterDao {
 
@@ -15,6 +16,16 @@ public interface CenterDao {
 	//목록 & 타입키워드 목록
 	List<CenterDto> selectList();
 	List<CenterDto> selectList(String type, String keyword);
+	
+	//페이징
+	int count(CenterListSearchVO vo);
+	int searchCount(CenterListSearchVO vo);
+	int listCount(CenterListSearchVO vo);
+	
+	//통합 검색 메소드(목록+검색)
+	List<CenterDto> selectList(CenterListSearchVO vo);
+	List<CenterDto> list(CenterListSearchVO vo);
+	List<CenterDto> search(CenterListSearchVO vo);
 	
 	//셀렉트원
 	CenterDto selectOne(int centerNo);
