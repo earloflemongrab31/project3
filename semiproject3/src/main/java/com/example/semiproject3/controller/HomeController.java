d2248adfeb911aa4b40e4656c3583f2c6c0db441
smy984 <smy984@naver.com>
2022-10-25 11:13:41 +0900

    회원 리스트 디자인 수정

--- semiproject3/src/main/java/com/example/semiproject3/controller/HomeController.java
+++ semiproject3/src/main/java/com/example/semiproject3/controller/HomeController.java
@@ -1,11 +1,17 @@
 package com.example.semiproject3.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.semiproject3.constant.SessionConstant;
 import com.example.semiproject3.repository.CartDao;
@@ -15,6 +21,14 @@ public class HomeController {
 	
 	@Autowired
 	CartDao cartDao;
+	
+//	맥북용
+	private final File directory = new File(System.getProperty("user.home")+"/upload/itemImage");
+//	화니꼬
+//	private final File directory = new File("C:/study/itemImage");
+//	D드라이브용
+//	private final File directory = new File("D:/study/itemImage");
+	
 	
 	@GetMapping("/")
 	public String home(
