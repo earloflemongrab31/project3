1268a1780649ad5a3f0c41e4d165e06e8b2a4255
earloflemongrab31 <earloflemongrab31@gmail.com>
2022-10-26 10:34:09 +0900

    리뷰-신고

--- semiproject3/src/main/java/com/example/semiproject3/controller/ReviewController.java
+++ semiproject3/src/main/java/com/example/semiproject3/controller/ReviewController.java
@@ -1,11 +1,10 @@
 package com.example.semiproject3.controller;
 
-import java.io.File;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.semiproject3.constant.SessionConstant;
-import com.example.semiproject3.entity.ImageDto;
+import com.example.semiproject3.entity.ReportDto;
 import com.example.semiproject3.entity.ReviewDto;
 import com.example.semiproject3.repository.ImageDao;
+import com.example.semiproject3.repository.ReportDao;
 import com.example.semiproject3.repository.ReviewDao;
 
 @Controller
@@ -28,6 +28,9 @@ public class ReviewController {
 	
 	@Autowired
 	private ImageDao imageDao;
+	
+	@Autowired
+	private ReportDao reportDao; 
 	
 	@GetMapping("/insert")
 	public String insert(
@@ -49,6 +52,37 @@ public class ReviewController {
 		reviewDao.insert(reviewDto);
 		
 		return "redirect:/item/buydetail?itemNo="+itemNo;
+	}
+	
+	//신고
+	@GetMapping("/report")
+	public String report(
+			@RequestParam int reviewNo,
+			Model model,
+			HttpSession session
+			) {
+		//세션을 이융해 리뷰번호를 담는다. 
+		session.setAttribute("reviewNo", reviewNo);
+		//하나 리뷰 정보를 불러와 화면에 뿌려준다. 
+		model.addAttribute("review",reviewDao.selectOne(reviewNo));
+		return "review/report";
+	}
+	@PostMapping("/report")
+	public String report(
+			@ModelAttribute ReportDto reportDto,
+			HttpSession session
+			) {
+		int reviewNo=(int)session.getAttribute("reviewNo");
+		//하나의 리뷰정보를 불러온다.
+		ReviewDto reviewDto=reviewDao.selectOne(reviewNo);
+		reportDao.insert(reportDto.builder()
+				.customerId(reviewDto.getCustomerId())
+				.reviewContent(reviewDto.getReviewContent())
+				.reportRadio(reportDto.getReportRadio())
+				.reportContent(reportDto.getReportContent())
+				.build());
+		
+		return "redirect:/";
 	}
 
 }
---
+++ semiproject3/src/main/java/com/example/semiproject3/repository/ReportDao.java
@@ -0,0 +1,9 @@
+package com.example.semiproject3.repository;
+
+import com.example.semiproject3.entity.ReportDto;
+
+public interface ReportDao {
+
+	//입력
+	void insert(ReportDto roportDto);
+}
