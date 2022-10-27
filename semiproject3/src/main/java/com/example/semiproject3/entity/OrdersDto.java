4e1d287fd0531fb288b10085fa5e7b42537561f5
smy984 <smy984@naver.com>
2022-10-27 14:57:27 +0900

    주문 구현 중

--- semiproject3/src/main/java/com/example/semiproject3/entity/OrdersDto.java
+++ semiproject3/src/main/java/com/example/semiproject3/entity/OrdersDto.java
@@ -10,22 +10,5 @@ public class OrdersDto {
 
 	private int ordersNo;
 	private String customerId;
-	private int itemNo;
-	private int addressNo;
-	private String customerName;
-	private String customerNick;
-	private String customerPhone;
-	private String itemName;
-	private String itemColor;
-	private String itemSize;
-	private String addressName;
-	private String customerPost;
-	private String customerHost;
-	private String customerDetailHost;
-	private int customerPoint; //0
-	private int itemCnt; //0
 	private int itemFee; //3000
-	private int customerMoney; //0
-	
-	
 }
