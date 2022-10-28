7daea26bfcab56ed96c930aef980bffd9a4c98a8
smy984 <106611123+smy984@users.noreply.github.com>
2022-10-28 13:30:26 +0900

    Update orders.sql

--- database(최종)/orders.sql
+++ database(최종)/orders.sql
@@ -2,15 +2,13 @@ CREATE TABLE orders(
 orders_no NUMBER PRIMARY KEY,
 customer_id REFERENCES customer(customer_id) ON DELETE SET NULL,
 item_no REFERENCES item(item_no) ON DELETE SET NULL,
-customer_name varchar2(21),
-customer_phone varchar2(11),
-address_name varchar2(11),
+address_name varchar2(21),
 address_phone varchar2(11),
-customer_post varchar2(6),
-customer_host varchar2(150),
-customer_detail_host varchar2(150),
+address_post varchar2(6),
+address_host varchar2(150),
+address_detail_host varchar2(150),
 pay_money NUMBER,
-delivery_fee NUMBER DEFAULT 2500,
+delivery_fee NUMBER check(delivery_fee >= 0),
 image_no NUMBER,
 item_name varchar2(100),
 item_price NUMBER,
@@ -18,7 +16,7 @@ item_size varchar2(100),
 item_color varchar2(100),
 item_cnt NUMBER,
 orders_time date
-)
+);
 
 DROP TABLE orders;
 
